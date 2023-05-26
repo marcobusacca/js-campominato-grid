1a09511afb5c9147a07ef45a309f70f71ba87264
marcobusacca <marcobusacca01@gmail.com>
2023-05-26 20:39:09 +0200

    [FEAT] Funzione createSquare

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
 "use strict";
+
+// FUNZIONE CHE GENERA DEI TAG HTML "DIV" CON CLASSE "SQUARE"
+function createSquare(){
+
+    // CREAZIONE ELEMENTO HTML "DIV" ED INSERIMENTO DENTRO UNA CONSTANTE
+    const square = document.createElement('div');
+
+    // INSERIMENTO CLASSE "SQUARE" ALL'ELEMENTO "DIV"
+    square.classList.add('square');
+
+    return square;
+}
